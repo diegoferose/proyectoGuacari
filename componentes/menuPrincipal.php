4fc8c2cc402ac8eef3b1dfd4f52bd78b7935e41c
diego <diegoferose@hotmail.com>
2019-04-23 00:02:55 -0500

    se divide el menu y el pie en archivos diferentes.

---
+++ componentes/menuPrincipal.php
@@ -0,0 +1,37 @@
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+      class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.html">NOMBRE S.A. </a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+            class="icon-user"></i> DIEGO FERNANDO ROSERO GIRALDO <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;"><i class="icon-bell"></i> Solicitudes</a></li>
+              <li><a href="javascript:;"><i class="icon-signout"></i>Cerrar sesion</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /navbar-inner -->
+</div>
+<!-- /navbar -->
+<div class="subnavbar">
+  <div class="subnavbar-inner">
+    <div class="container">
+      <ul class="mainnav">
+        <li class="active"><a href="index.html"><i class=" icon-home"></i><span>Inicio</span> </a> </li>
+        <li><a href="reports.html"><i class="icon-group"></i><span>¿Quienes somos?</span> </a> </li>
+        <li><a href="guidely.html"><i class="icon-book"></i><span></span>Politicas </a></li>
+      </ul>
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /subnavbar-inner -->
+</div>
+<!-- /subnavbar -->
