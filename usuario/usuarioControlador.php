a40a4b2dfa10a4abdfb0d98ab1311c234d785e63
diego <diegoferose@hotmail.com>
2019-04-24 23:27:23 -0500

    se eliminan los modelos y quedan funcionando con la funcion insertar del util.

--- usuario/usuarioControlador.php
+++ usuario/usuarioControlador.php
@@ -1,6 +1,8 @@
 <?php
 @session_start();
 include 'usuarioModelo.php';
+include '../util/utilModelo.php';
+$util = new utilModelo();
 $codigo = filter_input(INPUT_POST, 'codigo');
 $nombre = filter_input(INPUT_POST, 'nombre');
 $cedula = filter_input(INPUT_POST, 'cedula');
@@ -13,8 +15,15 @@ $correo = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 $activo = '0';//por defecto viene en 0 que es inactivo y 1 es activo
 $tipo = '2';//el tipo numero 2 es un usuario stadart ademas tenemos tipo 0 para los administradores y tipo 1 para los venderores.
-$modelo = new usuarioModelo();
-$modelo->insertar($codigo,$nombre, $cedula,$fechaNacimiento,$direccion, $celular,$codigoReferido,$usuario, $correo,$password,$activo,$tipo);
+
+//$campos es el nombre de los campos tal cual aparece en la base de datos
+$campos = array("codigo", "nombre", "cedula", "fechaNacimiento", "direccion", "telefono", "codigoReferido", "usuario", "correo", "password", "activo", "tipo");
+//$valores son los valores a almacenar
+$valores = array("$codigo","$nombre","$cedula","$fechaNacimiento","$direccion","$celular","$codigoReferido","$usuario","$correo","$password","$activo","$tipo");
+//la funcion insertar recive el nombre de la tabla y los dos arrays de campos y valores
+$nombreDeTabla = "usuario";
+$util -> insertar($nombreDeTabla,$campos, $valores) ;
+
 $_SESSION['mensajeOk']="ok";
 header('Location: ../index.php');
 ?>
