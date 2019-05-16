5a745f2f58f99e1e4ec536e17a7b62720ace2b57
diego <diegoferose@hotmail.com>
2019-05-16 15:47:57 -0500

    Se comenta un ejemplo de modificar en loguinControlador

--- seguridad/loguinControlador.php
+++ seguridad/loguinControlador.php
@@ -2,22 +2,29 @@
 @session_start();
 include '../util/utilModelo.php';
 $util = new utilModelo();
-$usuario = filter_input(INPUT_POST, 'username');
-$password = filter_input(INPUT_POST, 'ipassword');
 
-$nombreCampo = array("usuario","password");
-$valor = array("$usuario","$password");
-$tabla = "usuario";
-$result = $util -> mostrarregistros($tabla,$nombreCampo,$valor);
-$contador = 0;
-while ($fila = mysqli_fetch_array($result)) {
-    if ($fila != NULL) {
-        $_SESSION['usuario']=array($fila['codigo'],$fila['tipo']);
-        $contador++;
-    }
-}
+//$campos = array("codigoReferido","direccion");
+//$valores = array("sd33d21","calle 19 # 9-113");
+//$util->modificar('usuario',$campos,$valores,'id','1');
 
-include "../util/util.php";
-$util1 = new util();
-$util1 -> validarRuta(4);
+
+
+//$usuario = filter_input(INPUT_POST, 'username');
+//$password = filter_input(INPUT_POST, 'ipassword');
+//
+//$nombreCampo = array("usuario","password");
+//$valor = array("$usuario","$password");
+//$tabla = "usuario";
+//$result = $util -> mostrarregistros($tabla,$nombreCampo,$valor);
+//$contador = 0;
+//while ($fila = mysqli_fetch_array($result)) {
+//    if ($fila != NULL) {
+//        $_SESSION['usuario']=array($fila['codigo'],$fila['tipo']);
+//        $contador++;
+//    }
+//}
+//
+//include "../util/util.php";
+//$util1 = new util();
+//$util1 -> validarRuta(4);
 ?>
