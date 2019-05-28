b1d9e7a9127dbd9a014626c90cfc63158ed26b9c
diego <diegoferose@hotmail.com>
2019-05-28 10:14:20 -0500

    se crea vista de registrar pagos.

--- registrarPago/registroPagoVista.php
+++ registrarPago/registroPagoVista.php
@@ -1,1 +1,110 @@
 <?php
+    include "../util/util.php";
+    include_once "../util/utilModelo.php";
+    $utilModelo2 = new utilModelo();
+    $util = new util();
+//    $util->validarRuta(2);
+    $nombreCampo = array("tipo");
+    $valor = array("2");
+    $tabla = "usuario";
+//    echo "esta fue";
+    $result1 = $utilModelo2->mostrarregistros($tabla, $nombreCampo, $valor);
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>NOMBRE S.A.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+          rel="stylesheet">
+    <link href="../css/font-awesome.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/pages/dashboard.css" rel="stylesheet">
+    <link href="../css/pages/plans.css" rel="stylesheet">
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+<?php
+    include "../componentes/menuPrincipal.php";
+?>
+
+<div class="main">
+
+    <div class="main-inner">
+        <div class="container">
+
+            <div class="widget">
+                <div class="widget-header"> <i class="icon-money"></i>
+                    <h3>Registrar pagos.</h3>
+                </div>
+                <!-- /widget-header -->
+                <div class="widget-content">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Buscar persona:</label>
+                        <select class="selectpicker" name="lider" id="lider" data-show-subtext="true" data-live-search="true">
+                            <option value="null" data-subtext=" - Sin codigo">Ninguno</option>
+                            <?php
+                                
+
+                                while ($fila = mysqli_fetch_array($result1)) {
+                                    if ($fila != NULL) {
+                                        echo '<option value="' . $fila['codigo'] . '" data-subtext="' . $fila['codigo'] . '">' . $fila['nombre'] . '</option>';
+                                    }
+                                }
+//
+                            ?>
+                        </select>
+
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="firstname">Valor</label>
+                        <div class="controls">
+                            <input type="text" class="span6" id="firstname" >
+                        </div> <!-- /controls -->
+                    </div>
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-primary">Guaradar</button>
+                        <button class="btn">Cancelar</button>
+                    </div>
+                </div>
+                <!-- /widget-content -->
+            </div>
+
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /main-inner -->
+</div>
+
+<?php
+    include "../componentes/pie.php";
+?>
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="../js/jquery-1.7.2.min.js"></script>
+<script src="../js/excanvas.min.js"></script>
+<script src="../js/chart.min.js" type="text/javascript"></script>
+<script src="../js/bootstrap.js"></script>
+<script language="javascript" type="text/javascript" src="../js/full-calendar/fullcalendar.min.js"></script>
+<!--select picker-->
+<!--<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>-->
+<!--<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>-->
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css'>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+
+<script src="js/base.js"></script>
+
+</body>
+</html>
