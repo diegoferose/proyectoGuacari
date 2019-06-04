fb121cc03cbb18eb316c24c4e8d3a887ecd6e9fa
osdo1987 <oscar871220@hotmail.com>
2019-06-04 17:48:58 -0500

    Se realiza mejora al mensaje de restablecer contraseña

--- restaurarPassword/restaurarPasswordControlador.php
+++ restaurarPassword/restaurarPasswordControlador.php
@@ -10,17 +10,17 @@ include '../util/enviarCorreos.php';
 //$util->modificar('usuario',$campos,$valores,'id','1');
 				  $utilModelo = new utilModelo();
                   $util =new util();
-                  $enviarMail = new correosSmtp(); 
+                  $enviarMail = new correosSmtp();
 
 
 $usuario = filter_input(INPUT_POST, 'usuarioRestablecer');
 
 
-                  //variable busqueda del usuario               
+                  //variable busqueda del usuario
                   $nombreCampo = array("usuario");
 				  $valor = array("$usuario");
         		  $tabla = "usuario";
-                  $result = $utilModelo -> mostrarregistros($tabla,$nombreCampo,$valor);			            
+                  $result = $utilModelo -> mostrarregistros($tabla,$nombreCampo,$valor);
                   $fila = mysqli_fetch_array($result);
 
 if ($fila != null) {
@@ -28,43 +28,44 @@ if ($fila != null) {
 
                   $id=$fila[0];
                   $correoUsuario=$fila[10];
-                  
+
 
 
                   //Generador de nueva contraseña
-                    $key=$util->generarCodigo(); 
+                    $key=$util->generarCodigo();
 
                   //variables para el envio de correos
                   $destinatario = $correoUsuario;
                   $asunto = "Nueva Password GROW";
                   $mensaje="
 
-                  <table>
-                  <tr>
-                  <th colspan=2>
-					<h2>Nueva Password</h2>
-                  </th>
-                  </tr>
-                  <tr>
-					<td>Nombre Usuario</td>
-                    <td>$usuario</td>
-                  </tr
-                  <tr>
-					<td>Nueva Password</td>
-                    <td><b>$key<b></td>
-                  </tr>                  
+									<table style=\"border: 1px solid #000; border-radius: .25rem;font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif; color: #333;\">
+<tr>
+ <th colspan=\"2\" style=\" background: #77dd77;  color: #fff;border: none;\">
+  Nueva Password
+ </th>
+</tr>
+<td >
+ Nombre Usuario:
+</td>
+<td>
+ <b>$usuario</b>
+</td>
+<tr>
+ <td>Nueva Password: </td>
+ <td><b>$key</b></td>
+</tr>
+</table>
+<br><br>
 
 
-                  </table><br><br>
 
-
-
-                  Para ingresar nuevamente <a  href='localhost/proyectoGuacari/seguridad/loginVista.php' >aqui</a>
+                  Para ingresar nuevamente <a href=\"www.youtube.com\" >aqui</a>
                   ";
 
 
 
-                 //$enviarMail->enviarCorreos($destinatario,$asunto,$mensaje);
+                 $enviarMail->enviarCorreos($destinatario,$asunto,$mensaje);
 
                   //Variables de actuaizar Contraseña
                    $campos = array("password");
@@ -89,16 +90,16 @@ if ($fila != null) {
 				echo "Se ha enviado un mensaje a $destinatario Con su nueva contraseña";
 
 
-	
+
 }else{
 
 	echo "no se ha encontrado el usuario.Por favor verifique o comuniquese con el administrador";
 
 }
-                     
 
 
-                         
+
+
 
 
 ?>
