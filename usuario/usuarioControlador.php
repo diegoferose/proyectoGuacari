e523c7d61951f78d1955159aabd837b9fecba751
osdo1987 <oscar871220@hotmail.com>
2019-06-19 23:28:42 -0500

    Se agrega validacion para aumentar saldo cada vez que el vendedor registre un usuario

--- usuario/usuarioControlador.php
+++ usuario/usuarioControlador.php
@@ -19,6 +19,20 @@ $password = filter_input(INPUT_POST, 'password');
 $activo = '0';//por defecto viene en 0 que es inactivo y 1 es activo
 $tipo = '2';//el tipo numero 2 es un usuario stadart ademas tenemos tipo 0 para los administradores y tipo 1 para los venderores.
 
+
+
+$result = $utilModelo->consultarVariasTablas("tipo,saldo","usuario","codigo = '$codigoReferido'");
+
+while ($fila = mysqli_fetch_array($result)) {
+    if ($fila[0] == 1) {
+      $nuevoSaldo = $fila[1]+50000;
+      $campos = array("saldo");
+      $valores = array($nuevoSaldo);
+      $utilModelo->modificar('usuario',$campos,$valores,'codigo',$codigoReferido);
+
+        }
+}
+
 //$campos es el nombre de los campos tal cual aparece en la base de datos
 $campos = array("codigo", "nombre", "cedula", "fechaNacimiento","fechaDeIngreso", "direccion", "telefono", "codigoReferido", "usuario", "correo", "password", "activo", "tipo");
 //$valores son los valores a almacenar
